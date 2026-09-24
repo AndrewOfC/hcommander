b44b567f7bdb5f4c24255c5f248c481baf706c68
Andrew Page <andrew.e.page@gmail.com>
2026-09-24 09:16:58 -0400

    cleanup

---
+++ data/echos.sh
@@ -0,0 +1,25 @@
+##
+## used for isolating and testing when we
+## had an issue of enigo 'eating' the selected
+## charater from the text we were outputing
+##
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s-t"
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r-s"
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q-r"
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p-q"
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o-p"
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m-n-o"
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m-n"
+echo "a-b-c-d-e-f-g-h-i-j-k-l-m"
+echo "a-b-c-d-e-f-g-h-i-j-k-l"
+echo "a-b-c-d-e-f-g-h-i-j-k"
+echo 9
+echo 8
+echo 7
+echo 6
+echo 5
+echo 4
+echo 3
+echo 2
+echo 1
+echo 0
